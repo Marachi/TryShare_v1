31a33a8478206d4fa0ebb647e8c196d05b0d8c68
potaychuk <sviatoslavpotaychuk@gmail.com>
2016-04-22 11:51:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import yopackage.HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HelloWorld helloWorld = new HelloWorld();
+
+        int a = 1;
+    }
+}
---
+++ src/yopackage/HelloWorld.java
@@ -0,0 +1,12 @@
+package yopackage;
+
+/**
+ * Created by potaychuk on 22.04.2016.
+ */
+public class HelloWorld {
+    public HelloWorld(){
+        System.out.println("Hello World!");
+    };
+
+
+}
