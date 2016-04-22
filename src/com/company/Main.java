77ea26ae106c7ab62395f08e23d3a27033ca41ef
potaychuk <sviatoslavpotaychuk@gmail.com>
2016-04-22 12:50:18 +0300

    1) add static const with "new Helloworld"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import yopackage.HelloWorld;
 
 public class Main {
     static HelloWorld a =new HelloWorld();
+    static HelloWorld b =new HelloWorld();
     public static void main(String[] args) {
 	// write your code here
         HelloWorld helloWorld = new HelloWorld();
