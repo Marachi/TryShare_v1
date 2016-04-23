9cf7a0d2728e0a76487329e40c31fa9bb4148830
Sv.Potaychuk <sviatoslavpotaychuk@gmail.com>
2016-04-23 11:21:34 +0300

    Rejected commit: Default

--- src/mvc/Main.java
+++ src/mvc/Main.java
@@ -1,15 +1,25 @@
 package mvc;
 
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Initialization
-        Model model = new Model();
-        View view = new View();
-        Controller controller = new Controller(model, view);
-        // Run
-        controller.processUser();
+//        // Initialization
+//        Model model = new Model();
+//        View view = new View();
+//        Controller controller = new Controller(model, view);
+//        // Run
+//        controller.processUser();
+        Integer x;
+        Random random = new Random();
+       int a =(int)Math.random();
+
+        for(int i=0; i<100; i++) {
+            System.out.println(Math.random());
+        }
+
     }
 
 }
